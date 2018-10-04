cb95120dfaf7a604af22133229db2666c367bf17
shreykhetrapal <shrey.khetrapal@gmail.com>
2018-10-04 13:00:50 +0530

    Todo List complete with data source and delegate methods.

---
+++ Todoey/TodoListViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Shrey Khetrapal on 10/4/18.
+//  Copyright © 2018 Shrey Khetrapal. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController : UITableViewController {
+    
+    let itemArray = ["Buy this","Buy that","Buy Everything"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    //MARK - Table View Data Source Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+  
+    //The next function is returning a UITableViewCell , so we create a cell, fill it with our properties and then return it.
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    
+    //MARK - Table View Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //print(itemArray[indexPath.row])
+        
+        
+        //Checkmarking Cells
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+            }
+        else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true) //After selecting a row from the table view , the selection goes away
+        
+        
+    }
+    
+    
+
+
+}
+
